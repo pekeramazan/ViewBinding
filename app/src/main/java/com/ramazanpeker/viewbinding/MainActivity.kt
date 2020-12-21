ec039a8a5ca5ab71b8bb0a7890333d725b7412b2
pekeramazan <pekeramazan@gmail.com>
2020-12-21 21:20:59 +0300

    activity viewbinding

--- app/src/main/java/com/ramazanpeker/viewbinding/MainActivity.kt
+++ app/src/main/java/com/ramazanpeker/viewbinding/MainActivity.kt
@@ -2,10 +2,17 @@ package com.ramazanpeker.viewbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ramazanpeker.viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvtext.text=""
     }
 }
